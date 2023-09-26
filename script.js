64bb40f169941e9caac15fdeab6f8c4eb8a23dc1
Grace Nicole Arquiza <garquiza05@gmail.com>
2023-09-26 19:47:22 +0800

    Finished All Contents
    May add

--- script.js
+++ script.js
@@ -4,3 +4,30 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+function smoothScroll(target) {
+  const element = document.querySelector(target);
+  if (element) {
+    window.scrollTo({
+      top: element.offsetTop - 180,
+      behavior: "smooth",
+    });
+  }
+}
+
+const navLinks = document.querySelectorAll(".nav-links a, .menu-links a");
+navLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const target = link.getAttribute("href");
+    smoothScroll(target);
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("contact-form");
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    alert("Form submitted!");
+  });
+});
